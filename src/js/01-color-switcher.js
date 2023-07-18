7b70bc2a0d9b7f26a3e26ee8df97f3f42a37e476
NataliaMazurak <mazurak25@gmail.com>
2023-07-18 22:04:49 +0300

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+let intervalId = null;
+let isActive = false;
+
+startBtn.addEventListener('click', changeColor);
+
+function changeColor() {
+  if (isActive) {
+    return;
+  }
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  intervalId = setInterval(() => {
+    isActive = true;
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+    console.log(randomColor);
+  }, 1000);
+}
+
+stopBtn.addEventListener('click', stopChangeColor);
+
+function stopChangeColor() {
+  clearInterval(intervalId);
+  isActive = false;
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
