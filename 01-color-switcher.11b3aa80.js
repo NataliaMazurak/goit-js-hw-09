3c243c914abe1e438a1fa696cb2593cbae5cac61
NataliaMazurak <126176172+NataliaMazurak@users.noreply.github.com>
2023-07-23 13:13:48 +0000

    Deploying to gh-pages from @ NataliaMazurak/goit-js-hw-09@2f2b55517fd77d5a0f9c177392908c5106888bc3 🚀

--- 01-color-switcher.11b3aa80.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let d=null,n=!1;t.addEventListener("click",(function(){if(n)return;t.disabled=!0,e.disabled=!1,d=setInterval((()=>{n=!0;const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t,console.log(t)}),1e3)})),e.addEventListener("click",(function(){clearInterval(d),n=!1,t.disabled=!1,e.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.11b3aa80.js.map
